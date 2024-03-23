51623c19c2ab5b74312a6157df480eb812e737ad
Bhargav Madishetti <161111369+madishetti2000@users.noreply.github.com>
2024-03-24 02:28:41 +0530

    Add files via upload
    Body Mass Index calculator

---
+++ BMI.py
@@ -0,0 +1,7 @@
+height=input()
+weight=input()
+weight_as_int=int(weight)
+height_as_float=float(height)
+bmi=weight_as_int / (height_as_float*height_as_float) #can be written as (height_as_float**2)
+bmi_as_int=int(bmi)
+print(bmi_as_int)
